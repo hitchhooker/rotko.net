256c3ed583162e6f214d5115a215c5454f1cc289
hitchhooker <tommi@niemi.lol>
2021-03-12 01:44:10 -0300

    update

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,34 +1,64 @@
-<header class="py-12 text-4xl text-center">
-  Application Starter Template
-  <img src="/favicon.png" alt="Reepolee" class="py-4 mx-auto" />
-</header>
-
-<main>
-  <p>A starter template for Svelte Application, uses Routify file-based router, WindiCSS to compile TailwindCSS and Vite.</p>
-
-  <p>
-    <a href="https://svelte.dev">Svelte</a><br />
-    <a href="https://routify.dev">Routify</a><br />
-    <a href="https://vitejs.dev">Vite</a><br />
-    <a href="https://windicss.netlify.app">WindiCSS</a><br />
-  </p>
-
-  <p>
-    Kudos to all the respective authors, special thanks to <a href="https://github.com/jakobrosenberg">@jakobrosenberg</a> and
-    <a href="https://github.com/dominikg">@dominikg.</a>
-  </p>
-
-  <p>
-    <a href="/fails">Have a look at 404</a>
-  </p>
-
-  <p>
-    <a href="/axios-post">Axios example</a>
-  </p>
-
-  <h2>Should be on green and bold (global style). On blue for XL screen.</h2>
-</main>
-
+<div class="text-gray-200 bg-gray-900 min-h-screen">
+    <div class="max-w-6xl mx-auto px-5 py-24 ">
+      <div class="text-center mb-20">
+        <h1 class="title-font mb-4 text-4xl font-extrabold leading-10 tracking-tight sm:text-5xl sm:leading-none md:text-6xl">
+          <img src="images/logo.svg" alt="Rotko Networks" class="mx-auto w-6/12">
+        </h1>
+        <h3 class="text-bold leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+          Security without obscurity
+        <h3/>
+        <div class="flex mt-6 justify-center">
+          <div class="w-16 h-1 rounded-full bg-indigo-300 inline-flex"></div>
+        </div>
+      </div>
+      <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 ">
+        <div class="p-10 md:w-1/3 md:mb-0 mb-6 flex flex-col ">
+          <div class="pattern-dots-md gray-light">
+            <div class="rounded bg-gray-800 p-4 transform translate-x-6 -translate-y-6  "  >
+              <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-green-100 text-green-500 mb-5 flex-shrink-0 p-2">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01"></path></svg>                </div>
+              <div class="flex-grow ">
+                <h2 class=" text-xl title-font font-medium mb-3">Webdesign</h2>
+                <p class="leading-relaxed text-sm text-justify">
+                  We have over 15 years of experience designing web pages for very wide purposes. Using Figma we can deliver 
+                  specialized design that is the right fit for your branding. 
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="p-10 md:w-1/3 md:mb-0 mb-6 flex flex-col ">
+          <div class="pattern-dots-md gray-light">
+            <div class="rounded bg-gray-800 p-4 transform translate-x-6 -translate-y-6 ">
+              <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-blue-100 text-blue-500 mb-5 flex-shrink-0">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"></path></svg>
+              </div>
+              <div class="flex-grow">
+                <h2 class=" text-xl title-font font-medium mb-3">Web integration</h2>
+                <p class="leading-relaxed text-sm text-justify">
+                  We are capable of delivering any drawn webdesign programmed using latest CSS and JS frameworks to bring your website alive. 
+                  We are familiar with Bootstrap4 and TailwindCSS as well as VueJS and React.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="p-10 md:w-1/3 md:mb-0 mb-6 flex flex-col ">
+          <div class="pattern-dots-md gray-light">
+            <div class="rounded bg-gray-800 p-4 transform translate-x-6 -translate-y-6 ">
+              <div class="w-10 h-10 inline-flex items-center justify-centr rounded-full bg-yellow-100 text-yellow-500 mb-5 flex-shrink-0">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"></path></svg>
+              </div>
+              <div class="flex-grow">
+                <h2 class=" text-xl title-font font-medium mb-3">Developement Back-end</h2>
+                <p class="leading-relaxed text-sm text-justify">
+                 Thanks to PHP and frameworks like Laravel, Slim and Symphony we are capable of delivering dynamic websites,
+                 where data is safely saved in the server side database. 
+                </p>
+              </div>
+            </div>
+    </div>
+</div>
 <style>
   main {
     @apply p-8 text-xl;
