ebee7a90264b556e5b9b7340f6531a9d019e24f2
hitchhooker <tommi@niemi.lol>
2021-03-13 13:03:02 -0300

    add width and height to img tag

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -11,7 +11,9 @@
         <img
           src="images/logo.svg"
           alt="Rotko Networks"
-          class="mx-auto max-w-500px max-h-150px w-10/12 sm:w-8/12 md:w-6/12 mb-4"
+          width="500px"
+          height="150px"
+          class="mx-auto w-10/12 sm:w-8/12 md:w-6/12 mb-4"
         />
       </Heading>
       <p
